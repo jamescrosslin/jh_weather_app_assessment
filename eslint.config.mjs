a860804412496707f17a51a1acd0097d5236d28b
James Crosslin <jamesmscrosslin@gmail.com>
2025-03-05 10:27:03 -0800

    initial: server build

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { ignores: ['dist', 'node_modules'] },
+  { languageOptions: { globals: globals.browser } },
+  { rules:
+    {
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  stylistic.configs.customize({
+    'semi': true,
+    'quotes': 'single',
+    'blockSpacing': { before: 1, after: 1 },
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  }),
+];
