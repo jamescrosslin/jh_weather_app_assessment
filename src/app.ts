f7ee1e0067cb7c0ad311b4533adb8dff8133635d
James Crosslin <jamesmscrosslin@gmail.com>
2025-03-05 12:48:25 -0800

    feat: add CustomError and handling

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,19 @@
 import express from 'express';
+import type { Application, NextFunction, Request, Response } from 'express';
+import { CustomError } from './CustomError';
 
-const app = express();
+const app: Application = express();
 const port = 3000;
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: CustomError, _req: Request, res: Response, _next: NextFunction) => {
+  const status = err.statusCode || 500;
+
+  res.status(status).json({
+    message: err.message,
+    err,
+  });
+});
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
