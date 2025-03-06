c1cd0c4e1b4423919336123efa33e87ac89e6910
James Crosslin <jamesmscrosslin@gmail.com>
2025-03-05 16:49:01 -0800

    feat: add weather route

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,43 @@
 import express from 'express';
 import type { Application, NextFunction, Request, Response } from 'express';
-import { OpenWeatherAPI } from './OpenWeather';
+import { OpenWeatherAPI } from './services/OpenWeather/OpenWeatherAPI';
 import { loadVariableFromEnv } from './utils/loadVariableFromEnv';
+import { normalizeWeatherRequest } from './middleware/normalizeWeatherRequest';
 import { CustomError } from './CustomError';
+import { hasOpenWeatherInstantiated } from './middleware/hasOpenWeatherInstantiated';
 
 const app: Application = express();
 const port = 3000;
 
 app.locals.openWeather = new OpenWeatherAPI(loadVariableFromEnv('OPEN_WEATHER_API_KEY'));
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-app.use((err: CustomError, _req: Request, res: Response, _next: NextFunction) => {
-  const status = err.statusCode || 500;
+app.get('/weather',
+  hasOpenWeatherInstantiated,
+  normalizeWeatherRequest,
+  async (req: Request, res: Response) => {
+    const { latitude, longitude, exclude } = req.app.locals.normalizedWeatherRequests;
 
-  res.status(status).json({
-    message: err.message,
-    err,
+    const inputs = {
+      latitude,
+      longitude,
+      exclude,
+    };
+
+    const weather = await app.locals.openWeather.getWeatherByCoordinates(inputs);
+
+    res.status(200).json(weather);
   });
-});
+
+app.use(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  (err: CustomError, _req: Request, res: Response, _next: NextFunction) => {
+    const status = err.statusCode || 500;
+
+    res.status(status).json({
+      message: err.message,
+      err,
+    });
+  });
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
